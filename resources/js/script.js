b648ad00faac6ebcee9236a3fb298479d10598c5
Jindrich Mynarz <mynarzjindrich@gmail.com>
2017-09-21 16:06:06 +0200

    Add heading anchors sans jQuery

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,11 +1,21 @@
-(function ($) {
-  $(document).ready(function () {
-    $('h1,h2,h3,h4,h5,h6').filter('[id]').each(function () {
-      $(this).append(
-        '<a class="anchor" title="Link to the section" href="#' + 
-        $(this).attr('id') +
-        '">&nbsp;&para;</a>'
-      );
-    });
-  });
-})(jQuery);
+var addHeadingAnchors = function () {
+  var selector = [1, 2, 3, 4, 5, 6].map(function (idx) {
+        return "section[id] h" + idx.toString();
+      }).join(","),
+      sectionHeadings = Array.from(document.querySelectorAll(selector)),
+      sectionHeadingsLength = sectionHeadings.length;
+
+  for (var i = 0; i < sectionHeadingsLength; i++) {
+    var heading = sectionHeadings[i],
+        child = document.createElement("a");
+    child.className = "anchor";
+    child.title = "Link to the section";
+    child.href = "#" + heading.parentNode.id;
+    child.innerHTML = "&nbsp;&para;";
+    heading.appendChild(child);
+  }
+};
+
+(function () {
+  addHeadingAnchors();
+})();
